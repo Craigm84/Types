5649622029165688ab93ba231f8f4f8c2334ff66
Craigm84 <craigmorris2907@gmail.com>
2023-11-20 11:26:09 +0000

    init commit

---
+++ index2.js
@@ -0,0 +1,35 @@
+let age=39;
+
+console.log(age);
+
+age=40;
+
+console.log(age)
+
+let myname = "Craig"
+
+console.log(myname)
+
+myname = "Steve";
+
+console.log(myname)
+
+let muppet = false;
+
+console.log(muppet)
+
+muppet = true
+
+console.log(muppet)
+
+let demo;
+// Never had a value - therefore undefined
+console.log(demo)
+
+demo = null
+// Doesn't have a value
+console.log(demo)
+
+console.log(typeof muppet)
+
+console.log(typeof myname)
